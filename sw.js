9c45c80f653be1431647bd726854647fd8b2e2ea
Zhemmie <Karronz@pm.me>
2026-07-30 20:53:28 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='reading-desk-v11';
+const CACHE='reading-desk-v12';
 const ASSETS=['./','./index.html','./manifest.webmanifest',
   './icon-192.png','./icon-512.png','./icon-180.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
